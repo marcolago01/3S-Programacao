b1244683bea72a4e58b59974dc7bdf0fc3b09649
Marco Dionatan Lago <5586658@U702820.univali.br>
2018-05-02 22:04:32 -0300

    Exercicios Herança - Aula 8

--- Herança/src/Programacao.java
+++ Herança/src/Programacao.java
@@ -1,3 +1,8 @@
+
+import exercicio_1.Animal;
+import exercicio_1.Cachorro;
+import exercicio_1.Gato;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +19,16 @@ public class Programacao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Animal teste = new Animal();
+        System.out.println(teste.caminhar());
+        
+        teste = new Gato();
+        System.out.println(((Gato)teste).mia());
+        
+        teste = new Cachorro();
+        System.out.println(((Cachorro)teste).late());
+        
+        System.out.println(((Animal)teste).caminhar());
     }
     
 }
